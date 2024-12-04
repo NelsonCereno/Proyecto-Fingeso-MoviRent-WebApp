8ff25d72360c842906e9fe785dabf4fbbfa3b367
Nelson C <nelson.cereno@usach.cl>
2024-12-04 09:20:30 -0300

    Se sube archivo con spring inicial

---
+++ MoviRent-WebApp/src/main/java/Grupo_5/MoviRent_WebApp/MoviRentWebAppApplication.java
@@ -0,0 +1,13 @@
+package Grupo_5.MoviRent_WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviRentWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviRentWebAppApplication.class, args);
+	}
+
+}
---
+++ MoviRent-WebApp/src/test/java/Grupo_5/MoviRent_WebApp/MoviRentWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo_5.MoviRent_WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviRentWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

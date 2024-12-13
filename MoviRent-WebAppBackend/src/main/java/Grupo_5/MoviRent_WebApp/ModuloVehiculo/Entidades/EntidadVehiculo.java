900bd8922cdb725601b036310aac23d884d16ce1
Juanfra Echegoyen <juan.echegoyen@usach.cl>
2024-12-13 20:53:30 -0300

    arreglo ahora si
    Cambio controladorUsuario

--- MoviRent-WebAppBackend/src/main/java/Grupo_5/MoviRent_WebApp/ModuloVehiculo/Entidades/EntidadVehiculo.java
+++ MoviRent-WebAppBackend/src/main/java/Grupo_5/MoviRent_WebApp/ModuloVehiculo/Entidades/EntidadVehiculo.java
@@ -34,7 +34,123 @@ public class EntidadVehiculo {
     private String imagen;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getPatente() {
+        return patente;
+    }
 
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getCodigoAcriss() {
+        return codigoAcriss;
+    }
+
+    public void setCodigoAcriss(String codigoAcriss) {
+        this.codigoAcriss = codigoAcriss;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFechaRevision() {
+        return fechaRevision;
+    }
+
+    public void setFechaRevision(String fechaRevision) {
+        this.fechaRevision = fechaRevision;
+    }
+
+    public String getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(String kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public Boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }

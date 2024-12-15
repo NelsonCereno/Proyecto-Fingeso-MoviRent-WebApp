1942a9725c167ec3aa969184507b09f73c4e543a
ArielPinochet <ariel.pinochet@usach.cl>
2024-12-15 15:17:53 -0300

    Se arreglo la ensalada que quedo en el front
    buenos dias

--- MoviRent-WebAppFrontend/MoviRent/src/App.vue
+++ MoviRent-WebAppFrontend/MoviRent/src/App.vue
@@ -1,61 +1,36 @@
-//npm install -g npm
-//npm install axios
-//npm run dev
-
-// Funcionalidades de la página (JavaScript)
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 </script>
 
-// Estructura base de la página (HTML)
 <template>
-
   <header>
-
-    <!-- División del contenedor -->
     <div class="wrapper">
       <HelloWorld msg="Hola!" />
 
-      <!-- Navegación -->
+      <!-- Navegación Única -->
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/crear-reporte">Crear Reporte</RouterLink>
+        <RouterLink to="/SobreNosotros">Sobre Nosotros</RouterLink>
         <RouterLink to="/crear-usuario">Crear Usuario</RouterLink>
+        <RouterLink to="/crear-reporte">Crear Reporte</RouterLink>
+        <RouterLink to="/ver-reportes">Ver Reportes</RouterLink>
         <RouterLink to="/arriendos">Crear Arriendo</RouterLink>
+        <RouterLink to="/crear-vehiculo">Crear Vehículo</RouterLink>
         <RouterLink to="/vehiculos">Ver Vehículos</RouterLink> 
+
       </nav>
-      
     </div>
-     
-     <!-- Navegación -->
-     <nav>
-       <RouterLink to="/">Inicio</RouterLink>
-       <RouterLink to="/SobreNosotros">SobreNosotros</RouterLink>
-       <RouterLink to="/crear-reporte">Crear Reporte</RouterLink>
-       <RouterLink to="/crear-usuario">Crear Usuario</RouterLink>
-       <RouterLink to="/crear-vehiculo">Crear Vehículo</RouterLink>
-       <RouterLink to="/vehiculos">Ver Vehículos</RouterLink> 
-       <RouterLink to="/ver-reportes">Ver Reportes</RouterLink>
-     </nav>
-     
-   </div>
-   
-    <logos>
 
-    <img alt="Vue logo" class="logo" src="@/assets/estafaPiramidal.jpg" width="250" height="125" />
-    
-    </logos>
-
+    <div class="logos">
+      <img alt="Vue logo" class="logo" src="@/assets/estafaPiramidal.jpg" width="250" height="125" />
+    </div>
   </header>
 
   <div class="container-web">
     <RouterView />
   </div>
-
 </template>
 
-// Estética de la página (CSS)
 <style scoped>
 header {
   line-height: 1.5;
@@ -63,14 +38,14 @@ header {
   top: 0%;
   left: 0%;
   width: 100%;
-  position : absolute;
+  position: absolute;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color:#181818;
+  background-color: #181818;
 }
 
-logos {
+.logos {
   display: flex;
   width: 25%;
   height: 100%;
@@ -86,7 +61,6 @@ logos {
   background-color: #181818;
   justify-content: center;
   align-items: center;
-
 }
 
 .container-web {
@@ -147,7 +121,6 @@ nav a:first-of-type {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
   }
